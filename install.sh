64b9e90b3fc77e2a178c6faa95c7e89d787baee3
FlakeyKarma <flakeykarma21@gmail.com>
2022-11-27 12:05:44 -0700

    Adjusted the install script to ensure that the necessary packages are installed upon the rest of the install process taking off.

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Install necessary packages
+
+[ $(cat /etc/*-release | head -n1 | cut -d'"' -f2) != 'Arch' ] && echo "This project only runs for Arch linux at the moment!" && exit 1
+
+echo "These packages are necessary to run this script."
+sudo pacman -S route iptables netctl dhcp dhcpcd dnsmasq
+
 sudo cp src/configs/* /etc
